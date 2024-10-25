0a3fa054650478d152137f02afa161061f4d0c46
whoaskedssselery <ssselery@gmail.com>
2024-10-25 17:25:37 +0300

    ex 3 bubble sort

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func bubblesort(a []float64) []float64 {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if math.Abs(a[j]) > math.Abs(a[j+1]) {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+	return a
+}
 
 func main() {
-	fmt.Println("Hello, World !")
+	a := []float64{1, 2, 5, -2, 4, 10, 0}
+	sortedArray := bubblesort(a)
+	fmt.Println(sortedArray)
 }
